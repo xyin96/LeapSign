313fd8a28fe77c478779178bc18c4fed0921bdf8
xyin96 <yin530@gmail.com>
2014-09-20 19:17:38 -0400

    Major Update
    Spellcheck...? (Untested, but implemented)
    Voiced.

--- LeapSign/src/com/hgt/leapsign/LeapListener.java
+++ LeapSign/src/com/hgt/leapsign/LeapListener.java
@@ -1,16 +1,35 @@
 package com.hgt.leapsign;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import javax.sound.sampled.AudioFormat;
+import javax.print.attribute.standard.Media;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
@@ -21,15 +40,31 @@ import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.Listener;
 import com.leapmotion.leap.Vector;
+import com.swabunga.spell.engine.SpellDictionary;
+import com.swabunga.spell.engine.SpellDictionaryHashMap;
+import com.swabunga.spell.event.SpellCheckEvent;
+import com.swabunga.spell.event.SpellCheckListener;
+import com.swabunga.spell.event.SpellChecker;
+import com.swabunga.spell.event.StringWordTokenizer;
+import com.swabunga.spell.jedit.JazzyPlugin;
+import com.swabunga.spell.jedit.JazzySpellCheck;
 
-public class LeapListener extends Listener {
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+
+public class LeapListener extends Listener implements SpellCheckListener{
+	private final String API_KEY = "AIzaSyDUIHqw1okuAQ-XZCse3yQU3U23FVa90jE";
 	private Hand hand;
 	private String str, filter;
 	private StringBuilder word;
 	private int fcount = 0;
+	private Clip clip;
+	private SpellChecker spellCheck;
+	private BasicPlayer player;
 
 	public void onConnect(Controller controller) {
 		System.out.println("Connected");
+		
 
 	}
 
@@ -70,18 +105,73 @@ public class LeapListener extends Listener {
 		if (fcount > 45) {
 			if (str1 == null && word.length() > 0) {
 				filter = word.toString();
-				System.out.println("filter: " + filter);
 
-				URL t2v = null;
-
+				SpellDictionary dictionary = null;
 				try {
-					t2v = new URL(
-							"http://translate.google.com/translate_tts?ie=UTF-8&tl=ja&q"
-									+ URLEncoder.encode(filter));
-
-				} catch (Exception e) {
+					dictionary = new SpellDictionaryHashMap(new File(
+							"C:\\Users\\Xiaoyu\\workspace\\LeapSign\\words"));
+				} catch (FileNotFoundException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
 				}
-				;
+				if (dictionary != null) {
+					if (spellCheck == null) {
+						spellCheck = new SpellChecker(dictionary);
+						spellCheck.addSpellCheckListener(this);
+					}
+					spellCheck.checkSpelling(new StringWordTokenizer(filter));
+
+					System.out.println("filter: " + filter);
+
+					URL url = null;
+					try {
+						url = new URL(
+								"http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q="
+										+ URLEncoder.encode(filter));
+						URLConnection connection = url.openConnection();
+						connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+						InputStream in = new BufferedInputStream(
+								connection.getInputStream());
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						byte[] buf = new byte[1024];
+						int n = 0;
+						while (-1 != (n = in.read(buf))) {
+							out.write(buf, 0, n);
+						}
+						out.close();
+						in.close();
+						byte[] response = out.toByteArray();
+
+						FileOutputStream fos;
+						fos = new FileOutputStream(
+								"C:\\Users\\Xiaoyu\\workspace\\LeapSign\\tts.mp3");
+						fos.write(response);
+						fos.close();
+						
+						String pathToMp3 = "C:\\Users\\Xiaoyu\\workspace\\LeapSign\\tts.mp3";
+						try {
+							if(player == null){
+								player = new BasicPlayer();
+							}
+						    player.open(new URL("file:///" + pathToMp3));
+						    player.play();
+						} catch (BasicPlayerException | MalformedURLException e) {
+						    e.printStackTrace();
+						}
+						
+						
+
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 
 				LeapSign.jtf2.setText(filter);
 				word = new StringBuilder();
@@ -108,7 +198,7 @@ public class LeapListener extends Listener {
 		for (Finger f : hand.fingers()) {
 			fVel += f.tipVelocity().magnitude();
 		}
-		//System.out.println(fVel + "");
+		// System.out.println(fVel + "");
 		if (Math.sqrt(fVel) < 30) {
 			if (!hand.fingers().fingerType(Finger.Type.TYPE_INDEX).get(0)
 					.isExtended()
@@ -328,6 +418,22 @@ public class LeapListener extends Listener {
 		// System.out.println(thumb.cross(fingerDirs.get(1)));
 
 		return "I";
+	}
+
+	@Override
+	public void spellingError(SpellCheckEvent event) {
+		List suggestions = event.getSuggestions();
+		if (suggestions.size() > 0) {
+			System.out.println("MISSPELT WORD: " + event.getInvalidWord());
+			for (Iterator suggestedWord = suggestions.iterator(); suggestedWord
+					.hasNext();) {
+				System.out.println("\tSuggested Word: " + suggestedWord.next());
+			}
+		} else {
+			System.out.println("MISSPELT WORD: " + event.getInvalidWord());
+			System.out.println("\tNo suggestions");
+		}
+
 	}
 
 }
